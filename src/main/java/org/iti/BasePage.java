09c3df36f3eeb408bc7ac790570ae0b441cf3026
ahmedhat0 <ahmedmedhatadly@gmail.com>
2022-05-07 02:18:04 +0200

    First Commit , inc. Abstract Class and its implementation on BasePage

---
+++ src/main/java/org/iti/BasePage.java
@@ -0,0 +1,106 @@
+package org.iti;
+
+import org.jetbrains.annotations.NotNull;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+public class BasePage extends Page {
+
+    public BasePage(WebDriver driver) {
+        super(driver);
+    }
+
+    @Override
+    protected void selectItemInDropdownByVisibleText(By ByElement, String visibleText) {
+        Select select = new Select(driver.findElement(ByElement));
+        select.selectByVisibleText(visibleText);
+    }
+
+    @Override
+    protected void selectItemInDropdown(By ByElement, int index) {
+        Select select = new Select(driver.findElement(ByElement));
+        select.selectByIndex(index);
+    }
+
+    @Override
+    protected void selectItemInDropdown(By ByElement, String value) {
+        Select select = new Select(driver.findElement(ByElement));
+        select.selectByValue(value);
+    }
+
+    @Override
+    protected void clickOn(By ByElement) {
+        driver.findElement(ByElement).click();
+    }
+
+    @Override
+    protected void sendKeys(By ByElement, String text) {
+        driver.findElement(ByElement).sendKeys(text);
+    }
+
+    @Override
+    protected void clear(By ByElement) {
+        driver.findElement(ByElement).clear();
+    }
+
+    @Override
+    protected String getText(By ByElement) {
+        return driver.findElement(ByElement).getText();
+    }
+
+    @Override
+    protected WebElement getWebElement(By ByElement) {
+        return driver.findElement(ByElement);
+    }
+
+    @Override
+    protected void mouseActions(@NotNull String actionNeeded, By ByElement) {
+        Actions actions = new Actions(driver);
+        switch (actionNeeded.toLowerCase()) {
+            case "hover":
+                actions.moveToElement(driver.findElement(ByElement)).perform();
+                break;
+            case "doubleclick":
+                actions.doubleClick(driver.findElement(ByElement)).perform();
+                break;
+            case "rightclick":
+                actions.contextClick(driver.findElement(ByElement)).perform();
+                break;
+        }
+    }
+
+    @Override
+    protected boolean isElement(By ByElement, @NotNull String actionNeeded) {
+        switch (actionNeeded.toLowerCase()) {
+            case "enabled":
+                return driver.findElement(ByElement).isEnabled();
+            case "selected":
+                return driver.findElement(ByElement).isSelected();
+            default:
+                return driver.findElement(ByElement).isDisplayed();
+        }
+    }
+
+    @Override
+    protected void waitForElement(By ByElement, @NotNull String toBe, long timeOUTinMilliseconds) {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofMillis(timeOUTinMilliseconds));
+        switch (toBe.toLowerCase()) {
+            case "visible":
+                wait.until(ExpectedConditions
+                        .visibilityOfElementLocated(ByElement));
+                break;
+            case "clickable":
+                wait.until(ExpectedConditions
+                        .elementToBeClickable(ByElement));
+                break;
+        }
+    }
+
+}
---
+++ src/main/java/org/iti/Page.java
@@ -0,0 +1,58 @@
+package org.iti;
+
+import org.jetbrains.annotations.Nullable;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public abstract class Page {
+    WebDriver driver;
+
+    // ! Page Constructor
+    public Page(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    /**
+    ! Abstract Methods
+     */
+
+    protected abstract void selectItemInDropdownByVisibleText(By ByElement, String visibleText);
+
+    protected abstract void selectItemInDropdown(By ByElement, int index);
+
+    protected abstract void selectItemInDropdown(By ByElement, String value);
+
+    protected abstract void clickOn(By ByElement);
+
+    protected abstract void mouseActions(String actionNeeded, By ByElement);
+
+    protected abstract void sendKeys(By ByElement, String text);
+
+    protected abstract void clear(By ByElement);
+
+    protected abstract String getText(By ByElement);
+
+    protected abstract boolean isElement(By ByElement, String actionNeeded);
+
+    protected abstract WebElement getWebElement(By ByElement);
+
+    protected abstract void waitForElement(By ByElement, String toBe, long timeOutInSeconds);
+
+    /**
+     * <h4>This is a generic type.</h4>
+     * is used to create new Instance of any Page.
+     * @param pageNeeded This is the name of the Page we want to create.
+     * @return a constructor of the new Instance of the Page.
+     */
+
+    public <NeededPage extends BasePage> @Nullable NeededPage getInstance(Class<NeededPage> pageNeeded) {
+        try {
+            return pageNeeded.getDeclaredConstructor(WebDriver.class).newInstance(this.driver);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+}
